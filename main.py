459a761715b5e20a2143f06f5a201e080b18c8b1
Денис Громачихин <denisgromacihin@MacBook-Air-de-Denis.local>
2023-10-18 13:57:36 +0300

    -add player class

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import random
 
-class player():
+def fightRound2pl():
+    while player1.hp>0 or
+
+def fightRound3pl():
+    pass
+class Player():
     def __init__(self,hp, kz):
         self.hp = hp
         self.kz = kz
@@ -9,7 +14,24 @@ class player():
         if self.kz-random.randint(1,20)>=0:
             self.hp -= random.randint(1,8)
 
-player1 = player(hp=20, kz=14)
+    def defence(self):
+        self.kz+=3
+    def defenceoff(self):
+        self.kz -= 3
+
+    def hill(self):
+        pass
+
+class Npc():
+    def __init__(self,hp, kz):
+        self.hp = hp
+        self.kz = kz
+
+    def attak(self):
+        if self.kz-random.randint(1,20)>=0:
+            self.hp -= random.randint(1,8)
+
+player1 = Player(hp=20, kz=14)
 player1.attak()
 print(player1.hp)
 

1fc7b4164bfd4d30025d946a0ced6657dd39ec44
Денис Громачихин <denisgromacihin@MacBook-Air-de-Denis.local>
2023-10-18 12:48:19 +0300

    -create fightSystemBranch

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ class player():
         self.hp = hp
         self.kz = kz
 
-    def attac(self):
+    def attak(self):
         if self.kz-random.randint(1,20)>=0:
             self.hp -= random.randint(1,8)
 
 player1 = player(hp=20, kz=14)
-player1.attac()
+player1.attak()
 print(player1.hp)
 
 

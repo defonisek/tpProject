7cfc04a9e46fbeb64c8c39778a615e6c0e79ca11
defonisek <defonisek@mail.ru>
2023-10-21 23:59:28 +0300

    - started writing the main game code

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 import random
+import telebot
+from  telebot import types
+import time
 
-class player():
-    def __init__(self,hp, kz):
-        self.hp = hp
-        self.kz = kz
+bot = telebot.TeleBot('6682494061:AAFLZH7Qj32HYffI2UQdgpXxj0giooBe5iQ')
 
-    def attac(self):
-        if self.kz-random.randint(1,20)>=0:
-            self.hp -= random.randint(1,8)
-
-player1 = player(hp=20, kz=14)
-player1.attac()
-print(player1.hp)
-
-
-
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text == "/start": #запрашивает начало боя
+        #global enemy1
+        #global player1
+        #enemy1 = Npc(hp=10, kz=10, kz_flag=0, attack_flag=0)
+        #player1 = Player(hp=20, kz=14, kz_flag=0, hillC=1, attack_flag=0)
+        keyboard = types.InlineKeyboardMarkup()
+        key_oven = types.InlineKeyboardButton(text='В бой', callback_data='first_room')
+        keyboard.add(key_oven)
+        key_oven = types.InlineKeyboardButton(text='В бой', callback_data='fight_loop')
+        keyboard.add(key_oven)
+        key_oven = types.InlineKeyboardButton(text='В бой', callback_data='fight_loop')
+        keyboard.add(key_oven)
+        bot.send_message(message.from_user.id, text='в бой?', reply_markup=keyboard)
+    else:
+        bot.send_message(message.from_user.id, "Если хотите начать игру, то напишите '/start'.")
